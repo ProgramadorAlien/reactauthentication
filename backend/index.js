cc5b2af703eaeb84cfeb8910698092effb592a70
AlienMartinezDev <110763299+ProgramadorAlien@users.noreply.github.com>
2024-04-21 17:28:43 -0400

    Implement Register Component

--- backend/index.js
+++ backend/index.js
@@ -1,69 +1,54 @@
 const express = require('express');
 const mysql = require('mysql');
 const db = require('./database');
-
-
-
-// Attempt to acquire a connection from the pool to check the connection
-db.getConnection((err, connection) => {
-    if (err) {
-      console.error('Error connecting to database:', err);
-      return;
-    }
-    
-    console.log('Connected to database successfully!');
-    
-    // Release the connection back to the pool
-    connection.release();
-  });
-  
-
+const cors = require('cors'); // Import the cors middleware
 
 const app = express();
+
+// Use the cors middleware to enable CORS
+app.use(cors());
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
 // Register endpoint
 app.post('/register', (req, res) => {
-  const { username, password } = req.body;
-  // Insert user into database
-  db.query('INSERT INTO account (username, password) VALUES (?, ?)', [username, password], (err, result) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Failed to register user' });
-    } else {
-      console.log('User registered successfully');
-      res.status(200).json({ message: 'User registered successfully' });
-    }
-  });
+    const { username, password, email } = req.body;
+    // Insert user into database
+    db.query('INSERT INTO account (username, password, email) VALUES (?, ?, ?)', [username, password, email], (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ message: 'Failed to register user' });
+        } else {
+            console.log('User registered successfully');
+            res.status(200).json({ message: 'User registered successfully' });
+        }
+    });
 });
 
 // Login endpoint
 app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  // Check if user exists in database
-  db.query('SELECT * FROM account WHERE username = ? AND password = ?', [username, password], (err, results) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Failed to login' });
-    } else {
-      if (results.length > 0) {
-        res.status(200).json({ message: 'Login successful' });
-      } else {
-        res.status(401).json({ message: 'Invalid username or password' });
-      }
-    }
-  });
+    const { username, password } = req.body;
+    // Check if user exists in database
+    db.query('SELECT * FROM account WHERE username = ? AND password = ?', [username, password], (err, results) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ message: 'Failed to login' });
+        } else {
+            if (results.length > 0) {
+                res.status(200).json({ message: 'Login successful' });
+            } else {
+                res.status(401).json({ message: 'Invalid username or password' });
+            }
+        }
+    });
 });
 
-
-
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('Hello World');
-}); 
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,29 +1,155 @@
-import React from 'react';
-import './Register.css'; // Your CSS file for custom styling
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS
+import './Register.css';
 
-function App() {
+function RegisterForm() {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: ''
+  });
+
+  const [errors, setErrors] = useState({
+    username: '',
+    email: '',
+    password: ''
+  });
+
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const validateForm = () => {
+    let isValid = true;
+    const newErrors = {};
+
+    if (!formData.username.trim()) {
+      newErrors.username = 'Username is required';
+      isValid = false;
+    }
+
+    if (!formData.email.trim()) {
+      newErrors.email = 'Email is required';
+      isValid = false;
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      newErrors.email = 'Email is invalid';
+      isValid = false;
+    }
+
+    if (!formData.password.trim()) {
+      newErrors.password = 'Password is required';
+      isValid = false;
+    } else if (!/(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-zA-Z]).{8,}/.test(formData.password)) {
+      newErrors.password = 'Password must be 8 characters, at least one number, and one symbol';
+      isValid = false;
+    }
+    setErrors(newErrors);
+    return isValid;
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  }
+
+  useEffect(() => {
+    const registerUser = async () => {
+      if (formSubmitted) {
+        try {
+          const response = await fetch('http://localhost:3001/register', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+          });
+
+          if (response.ok) {
+            console.log('User registered successfully');
+            // Optionally, you can redirect the user to another page
+            // window.location.href = '/login'; // Example redirection
+          } else {
+            const data = await response.json();
+            console.error('Failed to register user:', data.message);
+          }
+        } catch (error) {
+          console.error('Error registering user:', error);
+        }
+
+        // Reset form fields after submission if needed
+        setFormData({
+          username: '',
+          email: '',
+          password: ''
+        });
+
+        // Reset form submission state
+        setFormSubmitted(false);
+      }
+    };
+
+    registerUser();
+  }, [formSubmitted, formData]); // Dependency array includes formSubmitted
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (validateForm()) {
+      setFormSubmitted(true);
+    } else {
+      console.log('Form validation failed');
+    }
+  }
+
   return (
     <div className="container mt-5">
       <div className="row justify-content-center">
         <div className="col-md-6">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-group row">
               <label htmlFor="username" className="col-sm-3 col-form-label text-left">UserName:</label>
               <div className="col-sm-9">
-                <input type="text" className="form-control" id="username" placeholder="Enter username" />
+                <input 
+                  type="text" 
+                  className={`form-control ${errors.username && 'is-invalid'}`} 
+                  id="username" 
+                  name="username" 
+                  value={formData.username} 
+                  onChange={handleInputChange} 
+                  placeholder="Enter username" 
+                />
+                {errors.username && <div className="invalid-feedback">{errors.username}</div>}
               </div>
             </div>
             <div className="form-group row">
               <label htmlFor="email" className="col-sm-3 col-form-label text-left">Email:</label>
               <div className="col-sm-9">
-                <input type="email" className="form-control" id="email" placeholder="Enter email" />
+                <input 
+                  type="email" 
+                  className={`form-control ${errors.email && 'is-invalid'}`} 
+                  id="email" 
+                  name="email" 
+                  value={formData.email} 
+                  onChange={handleInputChange} 
+                  placeholder="Enter email" 
+                />
+                {errors.email && <div className="invalid-feedback">{errors.email}</div>}
               </div>
             </div>
             <div className="form-group row">
               <label htmlFor="password" className="col-sm-3 col-form-label text-left">Password:</label>
               <div className="col-sm-9">
-                <input type="password" className="form-control" id="password" placeholder="Enter password" />
+                <input 
+                  type="password" 
+                  className={`form-control ${errors.password && 'is-invalid'}`} 
+                  id="password" 
+                  name="password" 
+                  value={formData.password} 
+                  onChange={handleInputChange} 
+                  placeholder="Enter password" 
+                />
+                {errors.password && <div className="invalid-feedback">{errors.password}</div>}
               </div>
             </div>
             <div className="form-group row">
@@ -38,4 +164,4 @@ function App() {
   );
 }
 
-export default App;
+export default RegisterForm;
